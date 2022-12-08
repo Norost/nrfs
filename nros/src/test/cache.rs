9c0ae2baa058aafd788ccb4dd5078db5ad3c361f
David Hoppenbrouwers <david@salt-inc.org>
2022-12-08 04:27:36 +0100

    Replace &mut s with &s in test::cache

--- nros/src/test/cache.rs
+++ nros/src/test/cache.rs
@@ -1,7 +1,7 @@
 use super::*;
 
 /// Clear the entire cache of the object store.
-async fn clear(s: &mut Nros<MemDev>) {
+async fn clear(s: &Nros<MemDev>) {
 	s.resize_cache(0, 0).await.unwrap();
 	let status = s.cache_status();
 	assert_eq!(status.global_usage, 0);
@@ -12,9 +12,9 @@ async fn clear(s: &mut Nros<MemDev>) {
 #[test]
 fn create_flush_get() {
 	run(|| async {
-		let mut s = new(MaxRecordSize::K1).await;
+		let s = new(MaxRecordSize::K1).await;
 		let id = s.create().await.unwrap().id();
-		clear(&mut s).await;
+		clear(&s).await;
 		s.get(id).await.unwrap();
 	})
 }
@@ -22,14 +22,14 @@ fn create_flush_get() {
 #[test]
 fn write_flush_read() {
 	run(|| async {
-		let mut s = new(MaxRecordSize::K1).await;
+		let s = new(MaxRecordSize::K1).await;
 
 		let obj = s.create().await.unwrap();
 		obj.resize(1000).await.unwrap();
 		obj.write(0, b"Hello, world!").await.unwrap();
 
 		let id = obj.id();
-		clear(&mut s).await;
+		clear(&s).await;
 		let obj = s.get(id).await.unwrap();
 
 		let mut buf = [0; b"Hello, world!".len()];
@@ -41,14 +41,14 @@ fn write_flush_read() {
 #[test]
 fn write_flush_read_offset_1000() {
 	run(|| async {
-		let mut s = new(MaxRecordSize::K1).await;
+		let s = new(MaxRecordSize::K1).await;
 
 		let obj = s.create().await.unwrap();
 		obj.resize(2000).await.unwrap();
 		obj.write(1000, b"Hello, world!").await.unwrap();
 
 		let id = obj.id();
-		clear(&mut s).await;
+		clear(&s).await;
 		let obj = s.get(id).await.unwrap();
 
 		let mut buf = [0; b"Hello, world!".len()];
@@ -60,14 +60,14 @@ fn write_flush_read_offset_1000() {
 #[test]
 fn write_flush_read_offset_1024() {
 	run(|| async {
-		let mut s = new(MaxRecordSize::K1).await;
+		let s = new(MaxRecordSize::K1).await;
 
 		let obj = s.create().await.unwrap();
 		obj.resize(2000).await.unwrap();
 		obj.write(1024, b"Hello, world!").await.unwrap();
 
 		let id = obj.id();
-		clear(&mut s).await;
+		clear(&s).await;
 		let obj = s.get(id).await.unwrap();
 		assert_eq!(obj.len().await.unwrap(), 2000);
 
@@ -81,7 +81,7 @@ fn write_flush_read_offset_1024() {
 #[test]
 fn write_flush_read_offset_1023() {
 	run(|| async {
-		let mut s = new(MaxRecordSize::K1).await;
+		let s = new(MaxRecordSize::K1).await;
 		s.store.verify_cache_usage();
 
 		let obj = s.create().await.unwrap();
@@ -91,7 +91,7 @@ fn write_flush_read_offset_1023() {
 		s.store.verify_cache_usage();
 
 		let id = obj.id();
-		clear(&mut s).await;
+		clear(&s).await;
 		let obj = s.get(id).await.unwrap();
 
 		let mut buf = [0; b"Hello, world!".len()];
@@ -103,7 +103,7 @@ fn write_flush_read_offset_1023() {
 #[test]
 fn write_flush_read_offset_10p6() {
 	run(|| async {
-		let mut s = new(MaxRecordSize::K1).await;
+		let s = new(MaxRecordSize::K1).await;
 		let obj = s.create().await.unwrap();
 
 		obj.resize(2_000_000).await.unwrap();
@@ -112,7 +112,7 @@ fn write_flush_read_offset_10p6() {
 		assert_eq!(l, 1000);
 
 		let id = obj.id();
-		clear(&mut s).await;
+		clear(&s).await;
 		let obj = s.get(id).await.unwrap();
 
 		let mut buf = [2; 1000];
