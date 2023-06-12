2cfe91c4ea2657a855af42a2c80fb47b1c1470c7
David Hoppenbrouwers <david@salt-inc.org>
2023-06-12 11:23:32 +0200

    Use chacha20poly1305 to ensure correct impl
    The Poly1305 key must be derived from a keystream as you should never
    reuse a key for this HMAC.

--- nros/src/data/cipher.rs
+++ nros/src/data/cipher.rs
@@ -7,7 +7,10 @@ use {
 		cipher::{KeyIvInit as _, StreamCipher as _},
 		XChaCha12,
 	},
-	poly1305::{universal_hash::KeyInit as _, Poly1305},
+	chacha20poly1305::{
+		aead::{AeadInPlace, NewAead},
+		Tag, XChaCha12Poly1305, XNonce,
+	},
 };
 
 n2e! {
@@ -24,35 +27,14 @@ pub(crate) struct Cipher {
 }
 
 impl Cipher {
-	/// Hash data.
-	fn hash(&self, data: &[u8]) -> [u8; 16] {
-		match self.ty {
-			CipherType::NoneXxh3 => xxh3_128(data).to_le_bytes(),
-			CipherType::XChaCha12Poly1305 => Poly1305::new_from_slice(&self.key1)
-				.unwrap()
-				.compute_unpadded(data)
-				.into(),
-		}
-	}
-
-	/// Apply keystream with the given key.
-	fn apply_inner(&self, key: &[u8; 32], nonce: &[u8; 24], data: &mut [u8]) {
+	/// Apply keystream for metadata, such as record headers.
+	pub fn apply_meta(&self, nonce: &[u8; 24], data: &mut [u8]) {
 		match self.ty {
 			CipherType::NoneXxh3 => {}
-			CipherType::XChaCha12Poly1305 => XChaCha12::new_from_slices(key, nonce)
+			CipherType::XChaCha12Poly1305 => XChaCha12::new_from_slices(&self.key2, nonce)
 				.unwrap()
 				.apply_keystream(data),
 		}
-	}
-
-	/// Apply keystream.
-	fn apply(&self, nonce: &[u8; 24], data: &mut [u8]) {
-		self.apply_inner(&self.key1, nonce, data)
-	}
-
-	/// Apply keystream for metadata, such as record headers.
-	pub fn apply_meta(&self, nonce: &[u8; 24], data: &mut [u8]) {
-		self.apply_inner(&self.key2, nonce, data)
 	}
 
 	/// Decrypt data in-place.
@@ -62,27 +44,37 @@ impl Cipher {
 		hash: &[u8; 16],
 		data: &mut [u8],
 	) -> Result<(), DecryptError> {
-		if !ct_eq(*hash, self.hash(data)) {
-			return Err(DecryptError);
+		match self.ty {
+			CipherType::NoneXxh3 => (&xxh3_128(data).to_le_bytes() == hash)
+				.then_some(())
+				.ok_or(DecryptError),
+			CipherType::XChaCha12Poly1305 => XChaCha12Poly1305::new_from_slice(&self.key1)
+				.unwrap()
+				.decrypt_in_place_detached(
+					XNonce::from_slice(nonce),
+					&[],
+					data,
+					Tag::from_slice(hash),
+				)
+				.map_err(|_| DecryptError),
 		}
-		self.apply(nonce, data);
-		Ok(())
 	}
 
 	/// Encrypt data in-place.
 	///
 	/// Returns the hash.
 	pub fn encrypt(&self, nonce: &[u8; 24], data: &mut [u8]) -> [u8; 16] {
-		self.apply(nonce, data);
-		self.hash(data)
+		match self.ty {
+			CipherType::NoneXxh3 => xxh3_128(data).to_le_bytes(),
+			CipherType::XChaCha12Poly1305 => XChaCha12Poly1305::new_from_slice(&self.key1)
+				.unwrap()
+				.encrypt_in_place_detached(XNonce::from_slice(nonce), &[], data)
+				.unwrap()
+				.into(),
+		}
 	}
 }
 
 /// Error returned when something went wrong while decrypting.
 #[derive(Clone, Debug)]
 pub struct DecryptError;
-
-/// Constant-time comparison.
-fn ct_eq(lhs: [u8; 16], rhs: [u8; 16]) -> bool {
-	u128::from_ne_bytes(lhs) == u128::from_ne_bytes(rhs)
-}
